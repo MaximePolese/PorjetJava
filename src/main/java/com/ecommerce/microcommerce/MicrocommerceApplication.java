e3d64b31c163239fecfeae50840c74f1d6e530d5
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-11-22 14:06:16 +0100

    first commit

---
+++ src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -0,0 +1,20 @@
+package com.ecommerce.microcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+//@Configuration
+//
+//@EnableAutoConfiguration
+//
+//@ComponentScan
+
+public class MicrocommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrocommerceApplication.class, args);
+    }
+
+}
